2450e5221fa827c966798e3498e5a4031823e35b
sashi799 <sashi799@gmail.com>
2015-06-20 13:56:58 +0530

    setup

---
+++ src/sas/java/practice/sample/Sample.java
@@ -0,0 +1,13 @@
+package sas.java.practice.sample;
+
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Sample program");
+
+	}
+
+}
